68f5f27af0aff644a2ee28eae9924c660db5b667
alokbaral09 <alokbaral09@gmail.com>
2025-10-15 23:34:43 +0530

    added/updated/delted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -3,6 +3,10 @@ package pages;
 public class LoginPage {
 	public static void main(String args[]) {
 		System.out.println("Hello Login page");
+		String un="admin";
+		String pwd="admin";
+		System.out.println("Logged in...");
+		
 
 	}
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class SearchPage {
+	
+	
+	public void serach() {
+		System.out.println("Search");
+	}
+
+}
